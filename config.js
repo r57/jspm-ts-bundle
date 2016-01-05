31aaa6db2086ae6ae5249cf1e9beb4a36dd20240
Petr Slajchrt <slajchrtp@avast.com>
2016-01-05 16:36:35 +0100

    Initial

---
+++ config.js
@@ -0,0 +1,58 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: false,
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  packages: {
+    "src": {
+      "main": "boot.ts",
+      "defaultExtension": "ts",
+      "meta": {
+        "*.ts": {
+          "loader": "ts"
+        },
+        "*.js": {
+          "loader": "ts"
+        }
+      }
+    }
+  },
+
+  map: {
+    "frankwallis/plugin-typescript": "github:frankwallis/plugin-typescript@2.4.5",
+    "lodash": "npm:lodash@3.10.1",
+    "ts": "github:frankwallis/plugin-typescript@2.4.5",
+    "github:frankwallis/plugin-typescript@2.4.5": {
+      "typescript": "npm:typescript@1.7.5"
+    },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    }
+  }
+});
---
+++ src/boot.ts
@@ -0,0 +1,5 @@
+import {Component} from './component'
+import _ from 'lodash'
+
+let cpm = new Component;
+_.identity(cpm);
